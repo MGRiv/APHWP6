f63c1c455bc8c98be0f523f39f757a5396b8179d
MGRiv <michael.rivera@cslab4-25.stuy.edu>
2014-12-01 12:30:40 -0500

    classwork

---
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -0,0 +1,22 @@
+public class OrderedSuperArray extends SuperArray{
+    
+    public void add(Object e){
+	boolean ret = true;
+	if(L.length > 0){
+	    add((L.size() / 2), e);
+	}else{
+	    resize(1);
+	    L[0] = e;
+	}
+    }
+    public void add(int index, Object e){
+	if(e.compareToIgnoreCase(L[index]) > 0){
+	    add((index + 1),e); 
+	}else if(e.compareToIgnoreCase(L[index]) < 0
+    }
+}
+//Check cases, (-), (+), (=)
+//add approrpiately, resize & shift
+//
+					 
+	   
