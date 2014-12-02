95aacaeed604f6e468b1eb4ffe2cbd2639c7d9b9
MGRiv <mgrivera501@gmail.com>
2014-12-02 00:54:42 -0500

    Had some difficulty w/ debug

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -1,22 +1,46 @@
 public class OrderedSuperArray extends SuperArray{
     
-    public void add(Object e){
-	boolean ret = true;
-	if(L.length > 0){
-	    add((L.size() / 2), e);
+    public OrderedSuperArray(){
+	super();
+    }
+    public OrderedSuperArray(int size){
+	super(size);
+    }
+    public void add(String e){
+	if(con > 0){
+	    int c = 0;
+	    System.out.println(e.compareTo(get(c)));
+	    while(c < con && e.compareTo(get(c)) > 0){
+		System.out.println(e.compareTo(get(c)));
+		c++;
+	    }
+	    if(c == size()){
+		super.add(e);
+	    }else{
+		super.add(c, e);
+	    }
 	}else{
-	    resize(1);
-	    L[0] = e;
+	    super.add(e);
 	}
     }
-    public void add(int index, Object e){
-	if(e.compareToIgnoreCase(L[index]) > 0){
-	    add((index + 1),e); 
-	}else if(e.compareToIgnoreCase(L[index]) < 0
+    public void add(int index, String e){
+	if(range(index)){
+	    add(e);
+	}else{
+	    throw new IndexOutOfBoundsException("invalid index");
+	}
     }
+    /*
+    public String set(int index, String e){
+	if(range(index)){
+	    super.remove(index);
+	    add(e);
+	    return e;
+	}else{
+	    throw new IndexOutOfBoundsException("invalid index");
+	}
+    }
+    */
 }
-//Check cases, (-), (+), (=)
-//add approrpiately, resize & shift
-//
 					 
 	   
--- 04OrderedSuperArray/SuperArray.java
+++ 04OrderedSuperArray/SuperArray.java
@@ -1,12 +1,12 @@
 public class SuperArray{
-    private Object[] L;
-    private int con;
+    private String[] L;
+    public int con = 0;
     
     public SuperArray(){
-	L = new Object[10];
+	L = new String[10];
     }
-    public SuperArray(String n){
-	L = new Object[n];
+    public SuperArray(int n){
+	L = new String[n];
     }
     public String toString(){
 	String r = "[";
@@ -17,81 +17,70 @@ public class SuperArray{
 	return r;
     }
     public boolean range(int index){
-	return !(index < 0 || index > size());
+	return !(index < 0 || index >= size());
     }
-    public void add(Object e){
-	if(L.length > 0){
-	    if(L[L.length - 1].equals(null)){
-		resize(L.length + 1);
-		L[L.length - 1] = e;
-	    }
-	    for(int i = 0; i < L.length; i++){
-		if(L[i].equals(null)){
-		    L[i] = e;
-		    break;
-		}
-	    }
-	}else{
-	    resize(1);
-	    L[0] = e;
-	}
-    }
-    public void add(int index, Object e){
-	if(range(index)){
+    public void add(String e){
+	if(con == L.length){
 	    resize(L.length * 2);
-	    for(int i = index+1; i < L.length; i++){
+	}
+	L[con] = e;
+	con++;
+    }
+    public void add(int index, String e){
+	if(range(index)){
+	    if(con == L.length){
+		resize(L.length * 2);
+	    }
+	    for(int i = con; i > index; i--){
 		L[i] = L[i - 1];
 	    }
 	    set(index,e);
+	    con++;
 	}else{
 	    throw new IndexOutOfBoundsException("invalid index");
 	}
     }
     public int size(){
-	con = 0;
-	for(int i = 0; i < L.length; i++){
-	    if(L[i] != null){
-		con++;
-	    }
-	}
 	return con;
     }
     public void resize(int newSize){
 	if(L.length > 0){
-	    Object[]temp = new Object[newSize];
+	    String[]temp = new String[newSize];
 	    for(int i = 0; i < L.length && i < newSize;i++){
 		temp[i] = L[i];
 	    }
 	    L = temp;
 	}else{
-	    L = new Object[1];
+	    L = new String[1];
 	}
     }
     public void clear(){
 	for(int i = 0; i < L.length; i++){
 	    L[i] = null;
 	}
+	con = 0;
     }
-    public Object remove(int index){
+    public String remove(int index){
 	if(range(index)){
-	    Object r = L[index];
+	    String r = L[index];
 	    for(int i = index; i < L.length - 1; i++){
 		L[i] = L[i + 1];
 	    }
 	    resize(L.length - 1);
+	    con--;
 	    return r;
 	}else{
 	    throw new IndexOutOfBoundsException("invalid index");
 	}
     }
-    public Object get(int index){
+    public String get(int index){
 	if(range(index)){
-	    return L[index - 1];
+	    return L[index];
 	}else{
 	    throw new IndexOutOfBoundsException("invalid index");
 	}
     }
-    public Object set(int index, Object e){
+    public String set(int index, String e){
 	if(range(index)){
 	    L[index] = e;
 	    return e;
