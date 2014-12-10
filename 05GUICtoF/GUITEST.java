ae329abf37912b5d622da653b68102b984a6431a
MGRiv <mgrivera501@gmail.com>
2014-12-10 18:27:12 -0500

    formula now correct

--- 05GUICtoF/GUITEST.java
+++ 05GUICtoF/GUITEST.java
@@ -7,11 +7,11 @@ public class GUITEST extends JFrame implements ActionListener{
     private JButton c,f;
     private JTextField t;
 
-    public Window(){
+    public GUITEST(){
 	this.setTitle("Temperature Conversion");
 	this.setSize(600, 200);
 	this.setLocation(100,100);
-	this.setdefaultCloseOperation(EXIT_ON_CLOSE);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 	pane = this.getContentPane();
 	pane.setLayout(new FlowLayout());
@@ -19,11 +19,33 @@ public class GUITEST extends JFrame implements ActionListener{
 	t = new JTextField(10);
 	c = new JButton("Celcius");
 	f = new JButton("Fahrenheit");
+
+	c.setActionCommand("Converttoc");
+	c.addActionListener(this);
+	f.setActionCommand("Converttof");
+	f.addActionListener(this);
+
 	pane.add(t);
 	pane.add(c);
 	pane.add(f);
     }
     public void actionPerformed(ActionEvent e){
-	String s = e.getActionCommand();
+	String action = e.getActionCommand();
+	if(action.equals("Converttof")){
+	    int temp;
+	    String s = t.getText();
+	    temp = Integer.parseInt(s);
+	    t.setText(Integer.toString(((temp * 9) / 5) + 32));
+	}
+	if(action.equals("Converttoc")){
+	    int temp;
+	    String s = t.getText();
+	    temp = Integer.parseInt(s);
+	    t.setText(Integer.toString(((temp - 32) * 5) / 9));
+	}
+    }
+    public static void main(String[] args){
+	GUITEST q = new GUITEST();
+	q.setVisible(true);
     }
 }
